5b5e89d1bf1a249d405c953c6507aa8cd1f82494
camoes666 <camoes666@gmail.com>
2026-04-23 19:55:27 +0900

    chore: release 0.1.1

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import nextPlugin from '@next/eslint-plugin-next'
+import tseslint from 'typescript-eslint'
+
+const eslintConfig = [
+  {
+    ignores: ['.next/**', 'node_modules/**', 'next-env.d.ts']
+  },
+  ...tseslint.configs.recommended,
+  {
+    files: ['*.js', '*.cjs'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off'
+    }
+  },
+  nextPlugin.configs['core-web-vitals']
+]
+
+export default eslintConfig
