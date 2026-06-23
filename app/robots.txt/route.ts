0c13f7c63f75a1111e26fc744362cab33e08eb55
camoes666 <camoes666@gmail.com>
2026-06-23 16:39:09 +0900

    chore: refresh daum webmaster verification pin

--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -6,7 +6,7 @@ export function GET() {
     'User-agent: *',
     'Allow: /',
     `Sitemap: ${baseUrl}/sitemap.xml`,
-    '#DaumWebMasterTool:b197faf4f49361368d61ea56b5de32fc0245ec0cdd94e83cc1d406e65c8bb965:KqMtrRaDniV96GOxiqaKXg=='
+    '#DaumWebMasterTool:cd00dc19f7d7b7914a4a50d2ffe2499efecb785acc56643df94bad74c0ee22a:KqMtrRaDniV96GOxiqaKXg=='
   ].join('\n')
 
   return new Response(body, {
